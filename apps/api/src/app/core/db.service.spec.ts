6efcd074d1721e2dbe458788809ff07797ff94ee
Sumit Parakh <sumit.thecoder@gmail.com>
2021-03-17 10:52:29 +0530

    test update

--- apps/api/src/app/core/db.service.spec.ts
+++ apps/api/src/app/core/db.service.spec.ts
@@ -3,10 +3,9 @@ import { TestBed } from '@angular/core/testing';
 import { DbService } from './db.service';
 
 describe('DbService', () => {
-  let service: DbService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(DbService);
+    TestBed.inject(DbService);
   });
 });
