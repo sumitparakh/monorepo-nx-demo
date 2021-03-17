e0eb00bc34238c1a9e873fca78ff3b5390ca09eb
Sumit Parakh <sumit.thecoder@gmail.com>
2021-03-17 10:53:51 +0530

    test update

--- apps/api/src/app/task.service.spec.ts
+++ apps/api/src/app/task.service.spec.ts
@@ -3,10 +3,8 @@ import { TestBed } from '@angular/core/testing';
 import { TaskService } from './task.service';
 
 describe('TaskService', () => {
-  let service: TaskService;
-
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(TaskService);
+    TestBed.inject(TaskService);
   });
 });
