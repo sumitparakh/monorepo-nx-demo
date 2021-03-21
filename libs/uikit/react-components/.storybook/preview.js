422eb73deb44799066098348e8f0d52de5b9d2a8
Sumit Parakh <sumit.thecoder@gmail.com>
2021-03-21 13:25:19 +0530

    feat: centered layout added

--- libs/uikit/react-components/.storybook/preview.js
+++ libs/uikit/react-components/.storybook/preview.js
@@ -1,4 +1,6 @@
 import { addDecorator } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
-
+export const parameters = {
+  layout: 'centered',
+};
 addDecorator(withKnobs);
