b7023302e7a1dbac0b139179d866faf8f0921424
Sumit Parakh <sumit.parakh@intelliswift.com>
2021-03-25 09:59:18 +0530

    changes

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -12,9 +12,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
+  const port = 3333;
   await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix +'.');
   });
 }
 
